e2818fdc22469568da2c983b9981df22f93f5d38
Chojiu <dev.cyth@gmail.com>
2022-01-25 14:38:51 +0100

    sql connection + routes

--- back/index.js
+++ back/index.js
@@ -1,14 +1,20 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3002
-const animalsRouter = require('./routes/animalsRouter')
-app.use(express.urlencoded({
-    extended : false
-}))
-const cors = require('cors')
-app.use(cors())
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3002;
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: false,
+  })
+);
 
-app.use('/api/animals', animalsRouter)
+require("dotenv").config();
+const cors = require("cors");
+app.use(cors());
 
+const animalsRouter = require("./routes/animalsRouter");
+const populationsRouter = require("./routes/populationsRouter");
+app.use("/api/animals", animalsRouter);
+app.use("/api/populations", populationsRouter);
 
-app.listen(port, console.log(`Server is listening on port ${port}`))
+app.listen(port, console.log(`Server is listening on port ${port}`));
